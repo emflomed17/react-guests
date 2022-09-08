03cf20c18472edc60c696da4afac27d8ea736677
Luis Morales <luis.morales.d1@gmail.com>
2022-09-07 21:29:00 -0500

    add test component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import GuestsMain from "./components/GuestsMain/GuestsMain";
+import Test from "./components/Test/Test";
 
 function App() {
   return (
     <div className="App">
       <h2>Lista de Invitados</h2>
-      <GuestsMain />
+      {/*<GuestsMain />*/}
+      <Test/>
     </div>
   );
 }
---
+++ src/components/Test/Test.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+// esto es un hook, use...
+import { useSelector, useDispatch } from 'react-redux';
+import { setUserNameAction } from '../../redux/feature/test/actions'
+
+export default function Test(){
+  const [username, setUsername] = useState( "");
+  const usernameRedux = useSelector((state) => state.test.userName);
+  const dispatch = useDispatch();
+
+  const handleOnChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleOnClick = () => {
+    // despachar action que despacha una funcion reducer
+    dispatch(setUserNameAction(username));
+  }
+
+  return(
+    <div>
+      <div className="Form">
+        <h2>Username: {usernameRedux}</h2>
+        <div>
+          <label id="usuario">Usuario:</label>
+          <input
+            type="text"
+            name="usuario"
+            value={username}
+            onChange={(e) => handleOnChange(e)}
+          />
+        </div>
+        <div>
+          <button onClick={handleOnClick}>Actualizar userName en redux</button>
+        </div>
+      </div>
+    </div>
+  )
+}

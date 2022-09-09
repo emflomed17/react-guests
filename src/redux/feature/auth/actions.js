02d9b3652f18d08553c3c7f8eda737d1b5a1f4aa
Luis Morales <luis.morales.d1@gmail.com>
2022-09-08 23:31:21 -0500

    update to use fetch in login

--- src/redux/feature/auth/actions.js
+++ src/redux/feature/auth/actions.js
@@ -1,17 +1,31 @@
 import { authenticated, loading, error, loggedOut } from './authSlice';
 
 export const authenticateUser = (credentials) => (dispatch) => {
-  const { email, password } = credentials;
   dispatch(loading());
-  if (email === 'foo@bar.com' && password === 'demo') {
-    const user = {
-      email: 'foo@bar.com',
-      name: 'Foo Bar'
+  fetch("https://631abbe6dc236c0b1ee329c1.mockapi.io/login",
+    {
+      method: "POST",
+      body: credentials
     }
-    dispatch(authenticated(user));
-  } else {
-    dispatch(error('Invalid credentials'));
-  }
+  ).then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      dispatch(authenticated(json));
+    })
+    .catch((errorResponse) => {
+      dispatch(error('Invalid credentials'));
+    });
+  // const { email, password } = credentials;
+  // dispatch(loading());
+  // if (email === 'foo@bar.com' && password === 'demo') {
+  //   const user = {
+  //     email: 'foo@bar.com',
+  //     name: 'Foo Bar'
+  //   }
+  //   dispatch(authenticated(user));
+  // } else {
+  //   dispatch(error('Invalid credentials'));
+  // }
 }
 
 export const logout = () => (dispatch) => {
